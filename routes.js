c8232a738f22224a58866c9bee1b9d402fd33fd6
sean <seansiddells@gmail.com>
2021-10-22 10:53:23 +1300

    push before pull

--- routes.js
+++ routes.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const server = require('./server')
 const router = express.Router()
 
 
 module.exports = router
 
+router.get('/:id', (req,res) => {
+  const input= req.params.id
+  res.render('question', input)
+})
